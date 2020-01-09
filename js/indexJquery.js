b2ca218d9fa5740c12ccec3c07de14463d47107c
Vlada-Hudyma <57041143+Vlada-Hudyma@users.noreply.github.com>
2020-01-10 00:35:45 +0200

    first commit

---
+++ js/indexJquery.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+	$("#menu").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+    });
+    
+    $('.three').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+      });
+});
+
+$(window).on('beforeunload', function() {
+  $(window).scrollTop(0);
+});
+
+
+var time = 2;
+$('number').each(function() {
+    var
+    i = 1,
+    num = $(this).data('num'),
+    step = 1000 * time / num,
+    that = $(this),
+    int = setInterval(function() {
+      if (i <= num) {
+        that.thml(i);
+      } else {
+        clearInterval(int);
+      }
+      i++;
+    }, step);
+  
+});
