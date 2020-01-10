e52a303069d725c68a8dc68712156f620c517b8d
Vlada-Hudyma <57041143+Vlada-Hudyma@users.noreply.github.com>
2020-01-11 00:10:45 +0200

    second commit

--- js/indexJquery.js
+++ js/indexJquery.js
@@ -21,19 +21,21 @@ $(window).on('beforeunload', function() {
 
 
 var time = 2;
-$('number').each(function() {
-    var
+$('.number').each(function(){
+  $('div').each(function(){
+    var 
     i = 1,
     num = $(this).data('num'),
     step = 1000 * time / num,
     that = $(this),
-    int = setInterval(function() {
+    int = setInterval(function(){
       if (i <= num) {
-        that.thml(i);
-      } else {
+        that.html(i);
+      }
+      else {
         clearInterval(int);
       }
       i++;
-    }, step);
-  
+    },step);
+  });
 });
