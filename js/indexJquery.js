720bbaa23dd5b17d57313586059b77bb73589e3c
Vlada-Hudyma <57041143+Vlada-Hudyma@users.noreply.github.com>
2020-01-11 20:20:34 +0200

    third commit

--- js/indexJquery.js
+++ js/indexJquery.js
@@ -6,36 +6,55 @@ $(document).ready(function(){
 		$('body,html').animate({scrollTop: top}, 1500);
     });
     
-    $('.three').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-      });
+     $('.three').slick({
+         dots: true,
+         infinite: true,
+         speed: 500,
+         fade: true,
+         cssEase: 'linear'
+       });
 });
+
+
 
 $(window).on('beforeunload', function() {
   $(window).scrollTop(0);
 });
 
 
-var time = 2;
+
+
+
+$(window).on('scroll', function(){
+  var w_top = $(window).scrollTop();
+  var e_top = $('.project__all').offset().top;
+
+  console.log(w_top + ' ' + e_top);
+
+  if(w_top >= 1398){
+
+    var time = 2;
 $('.number').each(function(){
-  $('div').each(function(){
-    var 
-    i = 1,
-    num = $(this).data('num'),
-    step = 1000 * time / num,
-    that = $(this),
-    int = setInterval(function(){
-      if (i <= num) {
-        that.html(i);
-      }
-      else {
-        clearInterval(int);
-      }
-      i++;
-    },step);
-  });
+  
+    var $item = $(this),
+      maxNum = +$item.data('num'),
+      step = time* 10000 / maxNum;
+
+    showNumber($item, step, maxNum);
 });
+
+    function showNumber($item, step, maxNum) {
+      var currentNum = +$item.html() + 1;
+    
+      if (currentNum <= maxNum) {
+        $item.html(Math.min(maxNum, currentNum));
+    
+        setTimeout(function() {
+          showNumber($item, step, maxNum)
+        }, step);
+      }
+    }
+  }
+})
+
+  
