403dd5301453fd415f6b19c1f53ffeec26adec4b
Vlada-Hudyma <57041143+Vlada-Hudyma@users.noreply.github.com>
2020-01-12 13:26:40 +0200

    thirt commit

--- js/indexJquery.js
+++ js/indexJquery.js
@@ -16,45 +16,41 @@ $(document).ready(function(){
 });
 
 
+var counterStart = false; 
+var e_top = $('.project__all').offset().top - $(window).height();
+console.log(e_top);
 
 $(window).on('beforeunload', function() {
   $(window).scrollTop(0);
 });
 
-
-
-
-
 $(window).on('scroll', function(){
   var w_top = $(window).scrollTop();
-  var e_top = $('.project__all').offset().top;
 
-  console.log(w_top + ' ' + e_top);
-
-  if(w_top >= 1398){
-
+  if(w_top >= e_top && !counterStart){
+    counterStart = true;
     var time = 2;
 $('.number').each(function(){
   
     var $item = $(this),
       maxNum = +$item.data('num'),
-      step = time* 10000 / maxNum;
+      step = Math.floor(time* 2000 / maxNum);
 
     showNumber($item, step, maxNum);
 });
 
-    function showNumber($item, step, maxNum) {
-      var currentNum = +$item.html() + 1;
     
-      if (currentNum <= maxNum) {
-        $item.html(Math.min(maxNum, currentNum));
-    
-        setTimeout(function() {
-          showNumber($item, step, maxNum)
-        }, step);
-      }
-    }
   }
 })
 
-  
+function showNumber($item, step, maxNum) {
+  var currentNum = +$item.html() + 1;
+
+  if (currentNum <= maxNum) {
+    $item.html(Math.min(maxNum, currentNum));
+
+    setTimeout(function() {
+      showNumber($item, step, maxNum)
+    }, step);
+  }
+}
